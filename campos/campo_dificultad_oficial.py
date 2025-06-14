fef5ae0d78a08581ab27fe9f2b021326f43e7ae4
paula-kleckers <pkleckers7817@gmail.com>
2025-06-14 14:47:23 +0200

    add perceived difficulty

--- campos/campo_dificultad_oficial.py
+++ campos/campo_dificultad_oficial.py
@@ -3,4 +3,5 @@ from campos.seleccion_roco_tipo_dif import opciones
 
 def input_dificultad_oficial(rocodromo, tipo_via):
     dificultades = opciones[rocodromo][tipo_via]
-    return st.selectbox("Dificultad oficial", dificultades, key="dificultad")
+    seleccion = st.selectbox("Dificultad oficial", dificultades, key="dificultad")
+    return seleccion, dificultades
--- campos/campo_dificultad_percibida.py
+++ campos/campo_dificultad_percibida.py
@@ -1,4 +1,17 @@
 import streamlit as st
+from campos.seleccion_roco_tipo_dif import opciones
 
-def input_dificultad_percibida():
-    return st.text_input("Dificultad percibida")
+def input_dificultad_percibida(dificultad_oficial, opciones):
+    opciones_finales = []
+
+    for d in opciones:
+        if d == dificultad_oficial:
+            opciones_finales.extend([
+                f"{d} (más fácil)",
+                f"{d}",
+                f"{d} (más difícil)"
+            ])
+        else:
+            opciones_finales.append(d)
+
+    return st.selectbox("Dificultad percibida", opciones_finales, key="dificultad_percibida")
