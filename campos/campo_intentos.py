4bc09c72cc78f832fd1deedc25a0a7896de2597d
paula-kleckers <pkleckers7817@gmail.com>
2025-06-14 19:01:21 +0200

    add help text for attemps field

--- campos/campo_intentos.py
+++ campos/campo_intentos.py
@@ -5,18 +5,22 @@ def input_intentos(tipo_ascension):
 
     if tipo_ascension == "👀 A vista" or tipo_ascension == "⚡ Flash":
         # Si es A vista o Flash, no se permite más de un intento
-        intentos = st.number_input("Intentos (en la fecha seleccionada)", value=1, disabled=True)
+        intentos = st.number_input("Intentos (en la fecha seleccionada)", value=1, disabled=True,
+                                   help="Selecciona el número de intentos realizados en la fecha seleccionada.")
 
     elif tipo_ascension == "✅ Completada":
         # Si es Completada, se permite cualquier número de intentos mayor que 1
-        intentos = st.number_input("Intentos (en la fecha seleccionada)", min_value=2, max_value=100, step=1)
+        intentos = st.number_input("Intentos (en la fecha seleccionada)", min_value=2, max_value=100, step=1,
+                                   help="Selecciona el número de intentos realizados en la fecha seleccionada.")
 
     elif tipo_ascension == '📚 Proyecto' or tipo_ascension == "🗑 Eliminada":
         # Si es Proyecto o Eliminada, no se permite ningún intento en la fecha seleccionada
-        intentos = st.number_input("Intentos (en la fecha seleccionada)", value=0, disabled=True)
+        intentos = st.number_input("Intentos (en la fecha seleccionada)", value=0, disabled=True,
+                                   help="Selecciona el número de intentos realizados en la fecha seleccionada.")
 
     else:
         # Si es Intentada o Top Rope, se permite cualquier número de intentos mayor que 1
-        intentos = st.number_input("Intentos (en la fecha seleccionada)", min_value=1, max_value=100, step=1)
+        intentos = st.number_input("Intentos (en la fecha seleccionada)", min_value=1, max_value=100, step=1,
+                                   help="Selecciona el número de intentos realizados en la fecha seleccionada.")
 
     return intentos
