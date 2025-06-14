58c281182452a1d2606a94b1f048ae3b3a7be7aa
paula-kleckers <pkleckers7817@gmail.com>
2025-06-14 17:36:29 +0200

    add emojis for route difficulty

--- campos/seleccion_roco_tipo_dif.py
+++ campos/seleccion_roco_tipo_dif.py
@@ -2,12 +2,13 @@ opciones = {
     "": {"": [""]},
     "OneMove": {
         "": [""],
-        "Boulder": ["", "Turquesa", "Verde", "Amarilla", "Blanca", "Azul", "Roja", "Negra"],
-        "Cuerda": ["", "Turquesa", "Verde", "Amarilla", "Blanca", "Azul", "Roja", "Negra"],
+        "Boulder": ["", "Turquesa", "🟩 Verde", "🟨 Amarilla", "⬜ Blanca", "🟦 Azul", "🟥 Roja", "⬛ Negra"],
+        "Cuerda": ["", "Turquesa", "🟩 Verde", "🟨 Amarilla", "⬜ Blanca", "🟦 Azul", "🟥 Roja", "⬛ Negra"],
     },
     "Climbat": {
         "": [""],
-        "Boulder": ["", "Blanca", "Amarilla", "Naranja", "Verde", "Azul", "Roja", "Negra"],
-        "Cuerda": ["", "4", "4+", "5", "5+", "6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b", "7b+", "7c", "7c+"],
+        "Boulder": ["", "⬜ Blanca", "🟨 Amarilla", "🟧 Naranja", "🟩 Verde", "🟦 Azul", "🟥 Roja", "⬛ Negra"],
+        "Cuerda": ["", "4", "4+", "5", "5+", "6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b", "7b+", "7c",
+                   "7c+"],
     },
 }
