6633fdf457c4010565a9d5e57c71307949418c78
paula-kleckers <pkleckers7817@gmail.com>
2025-07-23 00:21:36 +0200

    add El RoKo and kilter board

--- campos/seleccion_roco_tipo_dif.py
+++ campos/seleccion_roco_tipo_dif.py
@@ -1,14 +1,24 @@
 opciones = {
     "": {"": [""]},
-    "OneMove": {
+    "One Move": {
         "": [""],
-        "Boulder": ["", "Turquesa", "🟩 Verde", "🟨 Amarilla", "⬜ Blanca", "🟦 Azul", "🟥 Roja", "⬛ Negra"],
+        "Boulder": ["", "🩵 Turquesa", "🟩 Verde", "🟨 Amarilla", "⬜ Blanca", "🟦 Azul", "🟥 Roja", "⬛ Negra"],
         "Cuerda": ["", "🟩 Verde", "🟨 Amarilla", "🟥 Roja", "⬛ Negra"],
+        "Kilter": ["", "4a", "4b", "4c", "5a", "5b", "5c", "6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b",
+                   "7b+", "7c", "7c+", "8a", "8a+", "8b", "8b+", "8c", "8c+"],
     },
     "Climbat": {
         "": [""],
         "Boulder": ["", "⬜ Blanca", "🟨 Amarilla", "🟧 Naranja", "🟩 Verde", "🟦 Azul", "🟥 Roja", "⬛ Negra"],
         "Cuerda": ["", "4", "4+", "5", "5+", "6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b", "7b+", "7c",
                    "7c+"],
+    },
+    "El RoKo": {
+        "": [""],
+        "Boulder": ["", "🟩 Verde", "🟨 Amarilla", "🟧 Naranja", "🌸 Rosa", "🟪 Morada", "⬛ Negra"],
+        "Cuerda": ["Pendiente de añadir", "4", "4+", "5", "5+", "6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b", "7b+", "7c",
+                   "7c+"],
+        "Kilter": ["", "4a", "4b", "4c", "5a", "5b", "5c", "6a", "6a+", "6b", "6b+", "6c", "6c+", "7a", "7a+", "7b",
+                   "7b+", "7c", "7c+", "8a", "8a+", "8b", "8b+", "8c", "8c+"],
     },
 }
